a296e181489bcb48f0ae5e9c370c657b185e1eae
Dion Dokter <dion@tweedegolf.com>
2023-10-23 16:10:35 +0200

    Fixed solver formula. Added image generation

--- benches/hyperboloid_solver.rs
+++ benches/hyperboloid_solver.rs
@@ -2,16 +2,34 @@ use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criteri
 use tdoa_solver::{get_test_setup, solve};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let (entries, signal_speed, _) = get_test_setup();
+    let (mut entries, signal_speed, _) = get_test_setup();
 
     c.bench_with_input(BenchmarkId::new("Solve", 1), &1, |b, cycles| {
-        b.iter(|| black_box(solve(black_box(&entries), black_box(signal_speed), *cycles)))
+        b.iter(|| {
+            black_box(solve(
+                black_box(&mut entries),
+                black_box(signal_speed),
+                *cycles,
+            ))
+        })
     });
     c.bench_with_input(BenchmarkId::new("Solve", 10), &10, |b, cycles| {
-        b.iter(|| black_box(solve(black_box(&entries), black_box(signal_speed), *cycles)))
+        b.iter(|| {
+            black_box(solve(
+                black_box(&mut entries),
+                black_box(signal_speed),
+                *cycles,
+            ))
+        })
     });
     c.bench_with_input(BenchmarkId::new("Solve", 100), &100, |b, cycles| {
-        b.iter(|| black_box(solve(black_box(&entries), black_box(signal_speed), *cycles)))
+        b.iter(|| {
+            black_box(solve(
+                black_box(&mut entries),
+                black_box(signal_speed),
+                *cycles,
+            ))
+        })
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use glam::Vec3;
+use colorgrad::Gradient;
+use glam::{UVec2, Vec2, Vec3};
 use itertools::Itertools;
 use rand::Rng;
 
@@ -23,11 +24,12 @@ impl TdoaEntry {
 }
 
 pub fn solve(
-    entries: &[TdoaEntry],
+    entries: &mut [TdoaEntry],
     signal_speed_per_second: f32,
     cycles: usize,
 ) -> Vec<(Vec3, f32)> {
     assert!(!entries.is_empty(), "The entry list may not be empty");
+    sort_entries(entries);
 
     let test_locations = entries.iter().map(|entry| entry.location).chain(
         entries
@@ -112,8 +114,48 @@ pub fn solve(
         / location_clusters.len() as f32;
     location_clusters.retain(|cluster| cluster.1 > avg_weight);
 
-    location_clusters.sort_by(|l, r| l.1.total_cmp(&(r.1 as f32)).reverse());
+    location_clusters.sort_by(|l, r| l.1.total_cmp(&r.1).reverse());
     location_clusters
+}
+
+pub fn generate_image(
+    entries: &mut [TdoaEntry],
+    signal_speed_per_second: f32,
+    image_center: Vec3,
+    extents: Vec2,
+    resolution: UVec2,
+    gradient: &Gradient,
+    exposure: f32,
+    path: impl AsRef<std::path::Path>,
+) -> Result<(), image::ImageError> {
+    sort_entries(entries);
+
+    let mut imgbuf = image::ImageBuffer::new(resolution.x, resolution.y);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let x_frac = x as f32 / (resolution.x - 1) as f32;
+        let y_frac = y as f32 / (resolution.y - 1) as f32;
+
+        let value = multi_hyperboloid_value(
+            Vec3::new(
+                image_center.x + (x_frac * 2.0 - 1.0) * extents.y,
+                image_center.y + (y_frac * 2.0 - 1.0) * extents.y,
+                image_center.z,
+            ),
+            entries,
+            signal_speed_per_second,
+        );
+
+        let color = gradient.at((exposure / value) as f64);
+        *pixel = image::Rgba([
+            (color.r * 255.0) as u8,
+            (color.g * 255.0) as u8,
+            (color.b * 255.0) as u8,
+            (color.a * 255.0) as u8,
+        ]);
+    }
+
+    imgbuf.save(path)
 }
 
 fn random_unit_vector() -> Vec3 {
@@ -133,8 +175,16 @@ fn multi_hyperboloid_value(
 ) -> f32 {
     let mut acc = 0.0;
 
-    for (l, r) in entries.iter().tuple_combinations() {
-        acc += hyperboloid_value(test_location, l, r, signal_speed_per_second).abs();
+    let first_entry = entries[0];
+
+    for second_entry in entries[1..].iter() {
+        acc += hyperboloid_value(
+            test_location,
+            &first_entry,
+            second_entry,
+            signal_speed_per_second,
+        )
+        .abs();
     }
 
     acc
@@ -143,27 +193,27 @@ fn multi_hyperboloid_value(
 #[inline(always)]
 fn hyperboloid_value(
     test_location: Vec3,
-    l: &TdoaEntry,
-    r: &TdoaEntry,
+    first: &TdoaEntry,
+    second: &TdoaEntry,
     signal_speed_per_second: f32,
 ) -> f32 {
-    let l_distance = test_location.distance(l.location);
-    let r_distance = test_location.distance(r.location);
+    debug_assert_eq!(first.time_difference, 0.0);
 
-    let distance_difference = (l_distance - r_distance).abs();
-    let signal_distance =
-        (l.time_difference - r.time_difference).abs() * (signal_speed_per_second / 1_000_000_000.0);
+    let first_distance = test_location.distance(first.location);
 
-    distance_difference - signal_distance
+    let signal_distance = second.time_difference * (signal_speed_per_second / 1_000_000_000.0);
+    let second_distance = test_location.distance(second.location) - signal_distance;
+
+    first_distance - second_distance
 }
 
 #[doc(hidden)]
 pub fn get_test_setup() -> ([TdoaEntry; 4], f32, Vec3) {
     let mut entries = [
-        TdoaEntry::new(Vec3::new(0.0, 5.0, 0.0), 0.0),
+        TdoaEntry::new(Vec3::new(0.0, 10.0, 0.0), 0.0),
         TdoaEntry::new(Vec3::new(10.0, 0.0, 0.0), 0.0),
-        TdoaEntry::new(Vec3::new(10.0, 10.0, 10.0), 0.0),
-        TdoaEntry::new(Vec3::new(0.0, 0.0, 10.0), 0.0),
+        TdoaEntry::new(Vec3::new(10.0, 10.0, 5.0), 0.0),
+        TdoaEntry::new(Vec3::new(0.0, 0.0, 5.0), 0.0),
     ];
 
     let signal_speed = rand::thread_rng().gen_range(1.0..=300_000_000.0);
@@ -181,7 +231,7 @@ pub fn get_test_setup() -> ([TdoaEntry; 4], f32, Vec3) {
     let lowest_td = entries
         .iter()
         .map(|entry| entry.time_difference)
-        .min_by(|a, b| a.total_cmp(&b))
+        .min_by(|a, b| a.total_cmp(b))
         .unwrap();
 
     for entry in entries.iter_mut() {
@@ -191,13 +241,30 @@ pub fn get_test_setup() -> ([TdoaEntry; 4], f32, Vec3) {
     (entries, signal_speed, source_location)
 }
 
+fn sort_entries(entries: &mut [TdoaEntry]) {
+    assert!(
+        entries
+            .iter()
+            .all(|entry| entry.time_difference.is_finite() && entry.time_difference >= 0.0),
+        "The time_differences must all be finite and positive"
+    );
+    entries.sort_by(|l, r| f32::total_cmp(&l.time_difference, &r.time_difference));
+
+    let entry_min_time_difference = entries[0].time_difference;
+    for entry in entries.iter_mut() {
+        entry.time_difference -= entry_min_time_difference;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn hyperboloid_value_test() {
-        let (entries, signal_speed, source_location) = get_test_setup();
+        let (mut entries, signal_speed, source_location) = get_test_setup();
+
+        sort_entries(&mut entries);
 
         approx::assert_abs_diff_eq!(
             multi_hyperboloid_value(source_location, &entries, signal_speed),
@@ -218,11 +285,13 @@ mod tests {
     #[test]
     fn solve_test() {
         for _ in 0..50 {
-            let (entries, signal_speed, source_location) = get_test_setup();
+            let (mut entries, signal_speed, source_location) = get_test_setup();
 
-            println!("Testing setup with location: {source_location}, signal_speed: {signal_speed}");
+            println!(
+                "Testing setup with location: {source_location}, signal_speed: {signal_speed}"
+            );
 
-            let solved_location = solve(&entries, signal_speed, 10);
+            let solved_location = solve(&mut entries, signal_speed, 10);
 
             let any_correct_location = solved_location.iter().any(|(location, weight)| {
                 let distance = location.distance(source_location);
@@ -232,6 +301,40 @@ mod tests {
             });
 
             assert!(any_correct_location);
+        }
+    }
+
+    #[test]
+    fn test_image() {
+        let (mut entries, signal_speed, source_location) = get_test_setup();
+
+        let gradient = colorgrad::magma();
+        let exposure = 0.8;
+
+        generate_image(
+            &mut entries,
+            signal_speed,
+            Vec3::new(5.0, 5.0, source_location.z),
+            Vec2::new(7.0, 7.0),
+            UVec2::new(1024, 1024),
+            &gradient,
+            exposure,
+            format!("target/out exact.png"),
+        )
+        .unwrap();
+
+        for z in 0..=10 {
+            generate_image(
+                &mut entries,
+                signal_speed,
+                Vec3::new(5.0, 5.0, z as f32),
+                Vec2::new(7.0, 7.0),
+                UVec2::new(1024, 1024),
+                &gradient,
+                exposure,
+                format!("target/out {z}.png"),
+            )
+            .unwrap();
         }
     }
 }
